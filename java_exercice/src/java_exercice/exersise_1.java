e39a7571f8f33b4c59bc80bbf334a0d3e2ba8d9a
good <81326687+melat720@users.noreply.github.com>
2021-04-22 22:38:01 -0700

    initail commit

---
+++ java_exercice/src/java_exercice/exersise_1.java
@@ -0,0 +1,11 @@
+package java_exercice;
+
+public class exersise_1 {
+
+	public static void main(String[] args) {
+		String firstName="Hagos";
+		String lastName="Tebeges";
+		System.out.println(firstName + " " + lastName);
+	}
+
+}
